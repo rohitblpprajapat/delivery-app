e71024c56604d656da5c3eebe5dc57b68bce19bc
pikkugujjar <pikkugujjar952@gmail.com>
2024-12-20 16:03:45 +0530

    Initial commit

---
+++ food code/src/pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import './Home.css'
+import Header from '../../components/Navbar/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import { use } from 'react'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+const Home = () => {
+
+    const [category,setCategory] = useState("All");
+  return (
+    <div>
+     <Header/> 
+     <ExploreMenu category={category} setCategory={setCategory}/>
+     <FoodDisplay category={category}/>
+    </div>
+  )
+}
+
+export default Home
